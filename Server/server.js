43b105024d32a02ed49f9840751469534e0f5851
Andrew <lisowski54@gmail.com>
2015-05-21 22:16:00 -0700

    get it girl

---
+++ Server/server.js
@@ -0,0 +1,10 @@
+var http = require('http'),
+	router = require('./router');
+ 
+var app = router();
+http.createServer(app).listen(app.get('port'), function(){
+	if (process && process.env && process.env.NODE_ENV !== 'development') {
+	    console.log = function () {};
+	} 
+	console.log('Express server listening on port ' + app.get('port'));
+});
