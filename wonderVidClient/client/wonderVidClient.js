4bf039cb6fd049e746525cb4e381e0ecd3d937ee
Emmett <eharpe23@yahoo.com>
2015-05-21 23:32:52 -0700

    buttons

--- wonderVidClient/client/wonderVidClient.js
+++ wonderVidClient/client/wonderVidClient.js
@@ -151,6 +151,9 @@ Template.player.helpers({
 	currentVideo: function() {
 		return Session.get('currentVideo');
 	},
+	niceDescription: function() {
+		return linkify(Session.get('currentVideo').description);
+	},
 	sharedata: function() {
 		var url = 'https://www.youtube.com/watch?v=' + Session.get('currentVideo').videoId;
 		return {
@@ -387,3 +390,22 @@ renderVids = function() {
 
 	YT.load();   	
 };
+
+
+function linkify(inputText) {
+    var replacedText, replacePattern1, replacePattern2, replacePattern3;
+
+    //URLs starting with http://, https://, or ftp://
+    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+    //Change email addresses to mailto:: links.
+    replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+    replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+    return replacedText;
+}
